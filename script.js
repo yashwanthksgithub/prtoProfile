2e84672f740e4017b3e1cc3a6a10c7c942257af5
yashwanth ks <103103937+yashwanthksgithub@users.noreply.github.com>
2022-10-25 18:27:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hambur').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display='inline';
+        document.querySelector('.cross').style.display='none';
+    }
+    else{
+
+        document.querySelector('.ham').style.display='none';
+        
+        setTimeout(()=>{
+            document.querySelector('.cross').style.display='inline';
+        },500);
+
+    }
+
+});
